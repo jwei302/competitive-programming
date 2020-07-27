a47822a1a0418db53d3a0c14d8bfb4d566982a58
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-07-27 01:32:43 -0400

    Create skeleton.cpp

---
+++ skeleton.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define size(a) (int)(a).size()
+#define FOR(a) for(int i = 0; i < (a); ++i)
+#define read(a) FOR(size(a)) cin >> (a)[i]
+#define output(a) FOR(size(a)) cout << (a)[i] << " ";
+
+int main(){
+	
+	return 0;
+}
