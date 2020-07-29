1d5c6f0d885b3e660c1af7be60dceab5a0e26460
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-07-28 21:06:15 -0400

    Update skeleton.cpp

--- Resources/skeleton.cpp
+++ Resources/skeleton.cpp
@@ -1,17 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios::sync_with_stdio(0); cin.tie(0)
-#define ll long long
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef string str;
 
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<db, db> pd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<db> vd;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<str> vs;
+typedef vector<pi> vpi;
+typedef vector<pd> vpd;
+typedef vector<vi> vii;
+typedef vector<vl> vll;
+typedef vector<vd> vdb;
+typedef vector<vb> vbb;
+typedef vector<vc> vcc;
+typedef vector<vs> vss;
+
+#define mp make_pair
+#define f first
+#define s second	
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(a) (x).rbegin(), (x).rend()
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
 #define pb push_back
-#define all(a) (a).begin(), (a).end()
-#define size(a) (int)(a).size()
-#define FOR(a) for(int i = 0; i < (a); ++i)
-#define read(a) FOR(size(a)) cin >> (a)[i]
-#define output(a) FOR(size(a)) cout << (a)[i] << " ";
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define F0R(i, a, b) for(int i = (a); i < (b); ++i)
+#define FOR(i, a) F0R(i, 0, a)
+#define R0F(i, a, b) for(int i = (b)-1; i >= (a); --i)
+#define ROF(i, a) R0F (i, 0, a)
+#define trav(a, x) for (auto& a: x)
+
+const int MOD = 1e9+7;
+
+void setIn(str s) {freopen(s.c_str(), "r", stdin);}
+void setOut(str s) { freopen(s.c_str(), "w", stdout);}
+void unsyncIO() { ios::sync_with_stdio(0); cin.tie(0);}
+void setIO(str s= ""){
+	unsyncIO();
+	if(sz(s)) { setIn(s+".in"), setOut(s+".out");}
+}
 
 int main(){
-	
+
 	return 0;
 }
