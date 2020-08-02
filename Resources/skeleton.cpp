0149ae034f248253a94426f09350c38539def105
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-08-02 18:54:08 -0400

    Update skeleton.cpp

--- Resources/skeleton.cpp
+++ Resources/skeleton.cpp
@@ -47,6 +47,30 @@ typedef vector<vs> vss;
 #define ROF(i, a) R0F (i, 0, a)
 #define trav(a, x) for (auto& a: x)
 
+//input
+template<class A> void re(complex<A>& c);
+template<class A, class B> void re(pair<A,B>& p);
+template<class A> void re(vector<A>& v);
+template<class A, size_t SZ> void re(array<A,SZ>& a);
+
+template<class T> void re(T& x) { cin >> x; }
+void re(db& d) { str t; re(t); d = stod(t); }
+void re(ld& d) { str t; re(t); d = stold(t); }
+template<class H, class... T> void re(H& h, T&... t) { re(h); re(t...); }
+
+template<class A> void re(complex<A>& c) { A a,b; re(a,b); c = {a,b}; }
+template<class A, class B> void re(pair<A,B>& p) { re(p.f,p.s); }
+template<class A> void re(vector<A>& x) { trav(a,x) re(a); }
+template<class A, size_t SZ> void re(array<A,SZ>& x) { trav(a,x) re(a); }
+
+//output
+template<class A> void pr(A x) { cout << (x); }
+template<class H, class... T> void pr(const H& h, const T&... t) {
+	pr(h); pr(t...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class H, class... T> void ps(const H& h, const T&... t) {
+	pr(h); if (sizeof...(t)) pr(" "); ps(t...); }
+
 const int MOD = 1e9+7;
 
 void setIn(str s) {freopen(s.c_str(), "r", stdin);}
