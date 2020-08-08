457d32303a5d1fda3f15fec7e6afa39482ef0117
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-08-08 14:19:22 -0400

    Update pageant.cpp

--- Contests/USACO/Bronze/11-12/November/pageant.cpp
+++ Contests/USACO/Bronze/11-12/November/pageant.cpp
@@ -49,30 +49,6 @@ typedef vector<vs> vss;
 
 const int MOD = 1e9+7;
 
-//input
-template<class A> void re(complex<A>& c);
-template<class A, class B> void re(pair<A,B>& p);
-template<class A> void re(vector<A>& v);
-template<class A, size_t SZ> void re(array<A,SZ>& a);
-
-template<class T> void re(T& x) { cin >> x; }
-void re(db& d) { str t; re(t); d = stod(t); }
-void re(ld& d) { str t; re(t); d = stold(t); }
-template<class H, class... T> void re(H& h, T&... t) { re(h); re(t...); }
-
-template<class A> void re(complex<A>& c) { A a,b; re(a,b); c = {a,b}; }
-template<class A, class B> void re(pair<A,B>& p) { re(p.f,p.s); }
-template<class A> void re(vector<A>& x) { trav(a,x) re(a); }
-template<class A, size_t SZ> void re(array<A,SZ>& x) { trav(a,x) re(a); }
-
-//output
-template<class A> void pr(A x) { cout << (x); }
-template<class H, class... T> void pr(const H& h, const T&... t) {
-	pr(h); pr(t...); }
-void ps() { pr("\n"); } // print w/ spaces
-template<class H, class... T> void ps(const H& h, const T&... t) {
-	pr(h); if (sizeof...(t)) pr(" "); ps(t...); }
-
 void setIn(str s) {freopen(s.c_str(), "r", stdin);}
 void setOut(str s) { freopen(s.c_str(), "w", stdout);}
 void unsyncIO() { ios::sync_with_stdio(0); cin.tie(0);}
@@ -101,8 +77,8 @@ void dfs(int i, int j, vpi &sp){
 int main(){
 	setIO("pageant");
 	vpi sp1, sp2;
-	re(n, m);
-	FOR(i, n) FOR(j, m) re(a[i][j]);
+	cin >> n >> m;
+	FOR(i, n) FOR(j, m) cin>> a[i][j];
 	bool first = true, second = true;
 	FOR(i, n){
 		FOR(j, m){
@@ -123,7 +99,6 @@ int main(){
 				min = abs(a.f-b.f)+abs(a.s-b.s);
 		}
 	}
-	ps(min-1);
+	cout << min-1 << '\n';
 	return 0;
 }
-
