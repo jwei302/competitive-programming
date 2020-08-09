babd915af4ccdf1e046d21eb3c950f23eb52a445
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-08-09 13:39:23 -0400

    Update skeleton.cpp

--- Resources/skeleton.cpp
+++ Resources/skeleton.cpp
@@ -29,8 +29,8 @@ typedef vector<vs> vss;
 #define s second	
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
-#define rall(a) (x).rbegin(), (x).rend()
-#define rsz resize
+#define rall(a) (x).rbegin(), (x).readnd()
+#define rsz readsize
 #define ins insert
 #define cnt count
 #define ft front()
@@ -49,6 +49,30 @@ typedef vector<vs> vss;
 
 const int MOD = 1e9+7;
 
+//input
+template<class A> void read(complex<A>& c);
+template<class A, class B> void read(pair<A,B>& p);
+template<class A> void read(vector<A>& v);
+template<class A, size_t SZ> void read(array<A,SZ>& a);
+
+template<class T> void read(T& x) { cin >> x; }
+void read(db& d) { str t; read(t); d = stod(t); }
+void read(ld& d) { str t; read(t); d = stold(t); }
+template<class H, class... T> void read(H& h, T&... t) { read(h); read(t...); }
+
+template<class A> void read(complex<A>& c) { A a,b; read(a,b); c = {a,b}; }
+template<class A, class B> void read(pair<A,B>& p) { read(p.f,p.s); }
+template<class A> void read(vector<A>& x) { trav(a,x) read(a); }
+template<class A, size_t SZ> void read(array<A,SZ>& x) { trav(a,x) read(a); }
+
+//output
+template<class A> void print(A x) { cout << (x); }
+template<class H, class... T> void print(const H& h, const T&... t) {
+	print(h); print(t...); }
+void prints() { print("\n"); } // print w/ spaces
+template<class H, class... T> void prints(const H& h, const T&... t) {
+	print(h); if (sizeof...(t)) print(" "); prints(t...); }
+
 void setIn(str s) {freopen(s.c_str(), "r", stdin);}
 void setOut(str s) { freopen(s.c_str(), "w", stdout);}
 void unsyncIO() { ios::sync_with_stdio(0); cin.tie(0);}
@@ -58,6 +82,6 @@ void setIO(str s= ""){
 }
 
 int main(){
-	setIO();
+
 	return 0;
 }
