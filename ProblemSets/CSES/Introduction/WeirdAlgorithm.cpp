83318e8467666aab3f053480e987f55f0f695485
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-08-14 23:22:51 -0400

    Create WeirdAlgorithm.cpp

---
+++ ProblemSets/CSES/Introduction/WeirdAlgorithm.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef ll long long int
+
+int main(){
+	ll n;
+	cin >> n;
+	while(n!=1){
+		cout << n << " ";
+		if(n%2)
+			n=n*3+1;
+		else
+			n/=2;
+	}
+	cout << n << '\n';
+	return 0;
+}
+
