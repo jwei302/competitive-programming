9e6edce1090cf579204974c42edc75d9a3792701
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-08-31 00:33:55 -0400

    Add files via upload

--- Contests/Codechef/2020/DSA Learning Series/Linear Data Structures/chfqueue.cpp
+++ Contests/Codechef/2020/DSA Learning Series/Linear Data Structures/chfqueue.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ll long long
-#define ll long long
-#define mod 1000000007
+typedef long long int ll;
 
 void solve(){
 	ll n, k;
 	cin >> n >> k;
 	vector <ll> a(n);
-	for(int i = n-1; i >= 0; --i)
+	for(int i = 0; i < n; ++i)
 		cin >> a[i];
-		
-
+	ll ans = 1;
+	stack<ll> s;
+	for(int i = n-1; i >= 0; --i){
+		while(!s.empty()&&a[s.top()]>=a[i])
+			s.pop();
+		if(!s.empty())
+			ans = (ans*(s.top()-i+1))%((ll)(1e9+7));
+		s.push(i);
+	}
+	cout << ans << '\n';
 }
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int t;
-	cin >> t;
+	int t = 1;
+	//cin >> t;
 	while(t--){
 		solve();
 	}
