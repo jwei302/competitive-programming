57b7231acd5271495145a8166c5b53af3da5c9da
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-01 15:23:57 -0400

    Create tttt.cpp

---
+++ Contests/USACO/Bronze/17-18/US Open/tttt.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<db, db> pd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<db> vd;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<str> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+typedef vector<vi> vii;
+typedef vector<vl> vll;
+typedef vector<vd> vdb;
+typedef vector<vb> vbb;
+typedef vector<vc> vcc;
+typedef vector<vs> vss;
+
+#define f first
+#define s second	
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define part(x, n) (x).begin(), (x).begin()+n
+#define rpart(x, n) (x).rbegin(), (x).rbegin()+n
+#define rsz resize
+#define ins insert
+#define cnt count
+#define fr front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define LSOne(S) ((S)&-(S))
+
+#define F0R(i, a, b) for(int i = (a); i < (b); ++i)
+#define FOR(i, a) F0R(i, 0, a)
+#define R0F(i, a, b) for(int i = (b)-1; i >= (a); --i)
+#define ROF(i, a) R0F (i, 0, a)
+#define trav(a, x) for (auto& a: x)
+
+const int MOD = 1e9+7;
+
+//input
+template<class A> void read(complex<A>& c);
+template<class A, class B> void read(pair<A,B>& p);
+template<class A> void read(vector<A>& v);
+template<class A, size_t SZ> void read(array<A,SZ>& a);
+
+template<class T> void read(T& x) { cin >> x; }
+void read(db& d) { str t; read(t); d = stod(t); }
+void read(ld& d) { str t; read(t); d = stold(t); }
+template<class H, class... T> void read(H& h, T&... t) { read(h); read(t...); }
+
+template<class A> void read(complex<A>& c) { A a,b; read(a,b); c = {a,b}; }
+template<class A, class B> void read(pair<A,B>& p) { read(p.f,p.s); }
+template<class A> void read(vector<A>& x) { trav(a,x) read(a); }
+template<class A, size_t SZ> void read(array<A,SZ>& x) { trav(a,x) read(a); }
+
+//output
+template<class A> void print(A x) { cout << (x); }
+template<class H, class... T> void print(const H& h, const T&... t) {
+	print(h); print(t...); }
+void prints() { print("\n"); } // print w/ spaces
+template<class H, class... T> void prints(const H& h, const T&... t) {
+	print(h); if (sizeof...(t)) print(" "); prints(t...); }
+
+void setIn(str s) {freopen(s.c_str(), "r", stdin);}
+void setOut(str s) { freopen(s.c_str(), "w", stdout);}
+void unsyncIO() { ios::sync_with_stdio(0); cin.tie(0);}
+void setIO(str s= ""){
+	unsyncIO();
+	if(sz(s)) { setIn(s+".in"), setOut(s+".out");}
+}
+
+//brute force check to see if that row has winning one or duo, keep track of which pairs have one
+
+int main(){
+	setIO("tttt");
+	int one = 0, two = 0;
+	vcc board(3, vc(3));
+	read(board);
+	set<char> s;
+	map<str, int> mp;
+	FOR(i, 3){
+		s.clear();
+		FOR(j, 3)
+			s.ins(board[i][j]);
+		str st="";
+		trav(c, s)
+			st+=c;
+		mp[st]=1;
+	}
+	FOR(j, 3){
+		s.clear();
+		FOR(i, 3)
+			s.ins(board[i][j]);
+		str st="";
+		trav(c, s)
+			st+=c;
+		mp[st]=1;
+	}
+	s.clear();
+	FOR(i, 3)
+		s.ins(board[i][i]);
+	str st="";
+	trav(c, s)
+		st+=c;
+	mp[st] = 1;
+	s.clear();
+	FOR(i, 3)
+		ROF(j, 3)
+			s.ins(board[i][j]);
+	mp[st] = 1;
+	trav(key, mp){
+		if(sz(key.f)==1)
+			one++;
+		else if(sz(key.f)==2)
+			two++;
+	}
+	prints(one);
+	prints(two);
+	return 0;
+}
+
