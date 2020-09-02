a3d9eafbf46e7a6eb86deae4bea9d60dbcfb9c8f
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-02 11:01:24 -0400

    Update FenwickTreeBasic.cpp

--- Implementations/FenwickTreeBasic.cpp
+++ Implementations/FenwickTreeBasic.cpp
@@ -1,12 +1,21 @@
-vl ft[mxN+1];
-
-ll rsq(int j){
-	ll sum = 0;
-	for(; j; j-=LSOne(j))
-		sum += ft[j];
-	return sum;
-}
-void update(int i, ll v){
-	for(; i < n; i+=LSOne(i))
-		ft[i]+=v;
-}
+class FenwickTree{
+	private:
+		vl ft;
+	public:
+		FenwickTree(int m){
+			ft.assign(m+1, 0);
+		}
+		ll rsq(int j){
+			ll sum = 0;
+			for(; j; j-=LSOne(j))
+				sum += ft[j];
+			return sum;
+		}
+		ll rsq(int i, int j){
+			return rsq(j)-rsq(i-1);
+		}
+		void update(int i, ll v){
+			for(; i < sz(ft); i+=LSOne(i))
+				ft[i]+=v;
+		}
+};
