f9b5eff53b0c3b8eb519ca79d689574e2f829fde
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-02 21:24:58 -0400

    Create buckets.cpp

---
+++ Contests/USACO/Bronze/18-19/US Open/buckets.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<db, db> pd;
+typedef pair<str, str> ps;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<db> vd;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<str> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+typedef vector<ps> vps;
+typedef vector<vi> vii;
+typedef vector<vl> vll;
+typedef vector<vd> vdb;
+typedef vector<vb> vbb;
+typedef vector<vc> vcc;
+typedef vector<vs> vss;
+
+#define f first
+#define s second	
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define part(x, n) (x).begin(), (x).begin()+n
+#define rpart(x, n) (x).rbegin(), (x).rbegin()+n
+#define rsz resize
+#define ins insert
+#define cnt count
+#define fr front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define LSOne(S) ((S)&-(S))
+
+#define F0R(i, a, b) for(int i = (a); i < (b); ++i)
+#define FOR(i, a) F0R(i, 0, a)
+#define R0F(i, a, b) for(int i = (b)-1; i >= (a); --i)
+#define ROF(i, a) R0F (i, 0, a)
+#define trav(a, x) for (auto& a: x)
+
+const int MOD = 1e9+7;
+
+//input
+template<class A> void read(complex<A>& c);
+template<class A, class B> void read(pair<A,B>& p);
+template<class A> void read(vector<A>& v);
+template<class A, size_t SZ> void read(array<A,SZ>& a);
+
+template<class T> void read(T& x) { cin >> x; }
+void read(db& d) { str t; read(t); d = stod(t); }
+void read(ld& d) { str t; read(t); d = stold(t); }
+template<class H, class... T> void read(H& h, T&... t) { read(h); read(t...); }
+
+template<class A> void read(complex<A>& c) { A a,b; read(a,b); c = {a,b}; }
+template<class A, class B> void read(pair<A,B>& p) { read(p.f,p.s); }
+template<class A> void read(vector<A>& x) { trav(a,x) read(a); }
+template<class A, size_t SZ> void read(array<A,SZ>& x) { trav(a,x) read(a); }
+
+//output
+template<class A> void print(A x) { cout << (x); }
+template<class H, class... T> void print(const H& h, const T&... t) {
+	print(h); print(t...); }
+void prints() { print("\n"); } // print w/ spaces
+template<class H, class... T> void prints(const H& h, const T&... t) {
+	print(h); if (sizeof...(t)) print(" "); prints(t...); }
+
+void setIn(str s) {freopen(s.c_str(), "r", stdin);}
+void setOut(str s) { freopen(s.c_str(), "w", stdout);}
+void unsyncIO() { ios::sync_with_stdio(0); cin.tie(0);}
+void setIO(str s= ""){
+	unsyncIO();
+	if(sz(s)) { setIn(s+".in"), setOut(s+".out");}
+}
+
+//regular grid math, watch out for corner cases
+
+int main(){
+	setIO("buckets");
+	vcc grid(10, vc(10));
+	read(grid);
+	int x1, y1, x2, y2, x3, y3;
+	FOR(i, 10){
+		FOR(j, 10){
+			if(grid[i][j]=='B'){
+				x1 = j;
+				y1 = i;
+			}
+			if(grid[i][j]=='L'){
+				x2 = j;
+				y2 = i;
+			}
+			if(grid[i][j]=='R'){
+				x3 = j;
+				y3 = i;
+			}
+		}
+	}
+	int ans = abs(x1-x2)+abs(y1-y2)-1;
+	if(x1==x2&&x2==x3&&((y1<y3&&y3<y2)||(y1>y3&&y3>y2)))
+		ans += 2;
+	else if(y1==y2&&y2==y3&&((x1<x3&&x3<y2)||(x1>x3&&x3>x2)))
+		ans += 2;
+	prints(ans);
+	return 0;
+}
+
