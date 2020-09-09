2b4ba2244f054275923637f26e32dc3e1bbf885b
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-08 20:42:29 -0400

    Update rental.cpp

--- Contests/USACO/Silver/17-18/January/rental.cpp
+++ Contests/USACO/Silver/17-18/January/rental.cpp
@@ -16,6 +16,8 @@ bool cmp2(pair<ll, ll> a, pair<ll, ll> b){
 	return a.second>b.second;
 }
 int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	ifstream cin ("rental.in");
 	ofstream cout ("rental.out");
 	cin >> N >> M >> R;
