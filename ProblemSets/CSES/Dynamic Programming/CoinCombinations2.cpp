0a44bee4903e6022f4f8f60888e5bf27306cca93
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-13 20:25:17 -0400

    Add files via upload

---
+++ ProblemSets/CSES/Dynamic Programming/CoinCombinations2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define MOD (ll)1e9+7;
+
+const int mxN = 1e2, mxX = 1e6;
+ll N, X, coins[mxN], dp[mxN+1][mxX+1];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> X;
+	for(int i = 0; i < N; ++i){
+		cin >> coins[i];
+	}
+	dp[0][0] = 1;
+	for(int i = 1; i <= N; ++i){
+		for(int j = 0; j <= X; ++j){
+			dp[i][j] = dp[i-1][j];
+			if(j>=coins[i-1]){
+				(dp[i][j] += dp[i][j-coins[i-1]])%=MOD;
+			}
+		}
+	}
+	cout << dp[N][X] << '\n';
+	return 0;
+}
+
