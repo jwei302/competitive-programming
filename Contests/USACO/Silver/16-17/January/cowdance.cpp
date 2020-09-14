dc8ebdc2d73c282498bd0e20c8cc1ab2277711c9
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-13 22:13:13 -0400

    Update cowdance.cpp

--- Contests/USACO/Silver/16-17/January/cowdance.cpp
+++ Contests/USACO/Silver/16-17/January/cowdance.cpp
@@ -4,36 +4,24 @@ using namespace std;
 #define ll long long
 #define inf 1e9+7
 const int mxN = 1e4;
-int N, T, d[mxN], lb, rb, mid, tmp[mxN];
+int N, T, d[mxN], lb, rb, mid;
 
-//binary search the answer
+//binary search the answer and priority queue
 
 bool ok(int K){
-	int total = 0, curmn, nextmn = inf; 
+	int minTime = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
 	for(int i = 0; i < N; ++i){
-		if(i < K){
-			nextmn = min(tmp[i], nextmn);
+		if((int)pq.size() == K){
+			minTime = pq.top();
+			pq.pop();
 		}
-		tmp[i] = d[i];
-	}
-	int cnt = N-K+1, tmpK;
-	while(cnt--){
-		tmpK = K;
-		curmn = nextmn;
-		nextmn = inf;
-		total += curmn;
-		for(int j = 0; j < N && tmpK; ++j){
-			if(tmp[j]){
-				tmp[j] -= curmn;
-				tmpK--;
-			}
-			if(tmp[j]!=0){
-				nextmn = min(nextmn, tmp[j]);
-			}
+		if(minTime+d[i]>T){
+			return 0;
 		}
+		pq.push(minTime+d[i]);
 	}
-	total += *max_element(tmp, tmp+N);
-	return total <= T;
+	return 1;
 }
 int main(){
 	ifstream cin("cowdance.in");
