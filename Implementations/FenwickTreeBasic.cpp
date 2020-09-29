414c4c6f02771320e0fb511ada37d51d18c575ec
Jeffrey Wei <40704532+jwei302@users.noreply.github.com>
2020-09-28 23:26:16 -0400

    Update FenwickTreeBasic.cpp

--- Implementations/FenwickTreeBasic.cpp
+++ Implementations/FenwickTreeBasic.cpp
@@ -1,12 +1,13 @@
-ll ft[mxN+1];
-
-ll qry(int i){
-	ll sum = 0;
-	for(; i; i-= i&-i)
-		sum += ft[i];
-	return sum;
-}
-void upd(int i, ll v){
-	for(; i < N; i+=i&-i)
-		ft[i]+=v;
+struct FenwickTree{
+	ll ft[mxN+1];
+	ll qry(int i){
+		ll sum = 0;
+		for(; i; i-= i&-i)
+			sum += ft[i];
+		return sum;
+	}	
+	void upd(int i, ll v){
+		for(; i < N; i+=i&-i)
+			ft[i]+=v;
+	}
 }
